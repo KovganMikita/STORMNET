14b443b4d32de9551e634434027a6b6e6741bc31
Mikita.Kouhan <Kovgann@gmail.com>
2019-10-23 08:35:10 +0300

    Howework2

---
+++ src/Javarush/Test_esson_2.java
@@ -0,0 +1,11 @@
+package Javarush;
+
+import java.util.Scanner;
+
+public class Test_esson_2 {
+
+        public static void main(String[] args) {
+                    int a = 5;
+                    int b = 3;
+                    System.out.println(a + b);
+            System.out.println(a * b);}}
